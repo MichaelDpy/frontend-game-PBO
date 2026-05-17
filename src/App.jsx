26d7549fe217e9e93d5a50c69f64062bc396b3ae
naufalkhayri <50588635+naufalkhayri@users.noreply.github.com>
2026-05-17 20:42:27 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import AnimatedBackground from './components/AnimatedBackground';
-import MainMenu from './components/MainMenu';
+import GameContainer from './components/GameContainer';
 import { GameProvider } from './context/GameContext';
+import RotateScreen from './components/RotateScreen';
 
 function App() {
   return (
     <GameProvider>
+      <RotateScreen />
       <div className="relative min-h-screen overflow-hidden">
         <AnimatedBackground />
         <div className="relative z-10">
-          <MainMenu
-            onPlay={() => {}}
-            onHowToPlay={() => {}}
-            onCredit={() => {}}
-          />
+          <GameContainer />
         </div>
       </div>
     </GameProvider>
